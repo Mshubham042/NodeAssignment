02fe006c873dd52eecc1d025689ed321d1dc6b17
Mshubham042 <shubhammishra.dcs22n@vnsgu.ac.in>
2021-12-04 10:46:10 +0530

    data save

--- Nodejs_Assignment/Assignment_5/router/product.js
+++ Nodejs_Assignment/Assignment_5/router/product.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const product = require("../../Assignment_4/productdetail");
 const router = express.Router();
 const promodel =require("../modal/myproduct"); 
+const cmodel = require("../modal/mycmp");
 router.use(express.json());
 // const mongoose = require("mongoose");
 // mongoose.connect(process.env.mongo).then(()=>console.log("connected product"));
@@ -39,6 +41,24 @@ router.delete("/dltproduct",(req,res) => {
     });
     return res.json({data:"deleted data"+deletepro});
 });
+router.get("/getdata", async(req,res) => {
+
+     const pname = req.body.title;
+
+    promodel.findOne({title:pname} , (err,prodata) => {
+        if(err) console.log(err);
+        
+        cmodel.findOne({cmpid:prodata.company_id} , (err,mycmpdata) => {
+            if(err)
+           {  console.log(err); }
+      
+            console.log("All Products Of Company:"+ mycmpdata.prodid); 
+            console.log("Data of Company :"+mycmpdata);
+            return res.json({data:"details of Company :"+ mycmpdata}); 
+   });  
+            
+     });    
+})
 
 
 
